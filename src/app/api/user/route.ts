9c9de1e83488f1abe85c97ecd8e8d3269439fce3
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-15 01:22:11 +0530

    added backend user route

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,96 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import { connectDB } from '@/config/connectDb';
+import { User } from '@/models/user';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+    await connectDB();
+
+    const body = await req.json();
+
+    const { type } = body;
+
+    if (type === 'register') {
+        const { name, email, password } = body;
+
+        try {
+
+            const found = await User.findOne({ email });
+
+            if (found) {
+                return NextResponse.json({
+                    status: 400,
+                    message: "User already registered"
+                });
+            }
+
+            const hashed = await bcrypt.hash(password, 10);
+
+            const newUser = new User({
+                name, email, password: hashed
+            });
+
+            await newUser.save();
+
+            return NextResponse.json({
+                status: 200,
+                message: "User Registered",
+                newUser
+            });
+        } catch (err) {
+            return NextResponse.json({
+                status: 500,
+                message: "Something went wrong"
+            });
+        }
+    }
+    else{
+        const { email, password } = body;
+
+        try {
+            const found = await User.findOne({email});
+
+            if (!found) {
+                return NextResponse.json({
+                    status: 404,
+                    message: "User not found"
+                });
+            }
+
+            const matched = await bcrypt.compare(password, found.password);
+
+            if(!matched){
+                return NextResponse.json({
+                    status: 400,
+                    message: "Incorrect Password"
+                });
+            }
+
+            const secret = process.env.JWT_SECRET as string;
+
+            const token = jwt.sign({id: found._id}, secret, {expiresIn: '1d'});
+
+            const res = NextResponse.json({
+                status: 201,
+                success: true
+            });
+
+            res.cookies.set('token', token, {
+                sameSite: 'strict',
+                httpOnly: true,
+                secure: true,
+                maxAge: 86400
+            });
+
+            return res;
+            
+        } catch (err) {
+            console.error(err);
+            return NextResponse.json({
+                status: 500,
+                message: "Something went wrong"
+            });
+        }
+    }
+}
