f66a55bc39f26a520b250a64985a44a894110805
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-22 02:45:35 +0530

    added backend feedback data fetching route

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { FaPowerOff } from "react-icons/fa";
 import axios from "axios";
 import { useEffect, useState } from "react"
 import {
@@ -9,13 +10,18 @@ import {
   ModalTrigger,
 } from '@/components/ui/animated-modal';
 import InterviewBoxPast from "@/components/InterviewBoxPast";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerTrigger,
-} from '@/components/ui/drawer';
 import { useRouter } from "next/navigation";
 import { toast } from 'sonner';
+
+interface HIST {
+  name: string,
+  InterviewPreferrence: string,
+  role: string,
+  company: string,
+  experience: string,
+  feedback: string,
+  questions: string
+}
 
 function page() {
 
@@ -23,18 +29,29 @@ function page() {
   const [generating, setGenerating] = useState(false);
   const [data, setData] = useState<{ id: string, name: string, email: string }>();
   const [role, setRole] = useState('');
+  const [history, setHistory] = useState<HIST[] | []>([]);
   const [company, setCompany] = useState('');
   const [experience, setExperience] = useState('');
+  const [name, setName] = useState('');
   const [InterviewPreferrence, setInterviewPreferrence] = useState('');
   const router = useRouter();
 
   const getData = async () => {
     try {
       const res = await axios.get('/api/user');
-      //console.log(res.data.found);
+      console.log(res.data.found);
 
       if (res.data.status === 200) {
         setData(res.data.found);
+        setName(res.data.found.name);
+
+        try {
+          const get = await axios.get(`/api/feedback-history?id=${res.data.found._id}`);
+          //console.log(get.data);
+          setHistory(get.data.found);
+        } catch (err) {
+          
+        }
         setLoaded(true);
       }
     } catch (err) {
@@ -45,24 +62,6 @@ function page() {
   useEffect(() => {
     getData();
   }, []);
-
-  const array = [
-    {
-      role: 'frontend'
-    },
-    {
-      role: 'backend'
-    },
-    {
-      role: 'fullstack'
-    },
-    {
-      role: 'java'
-    },
-    {
-      role: 'MERN'
-    },
-  ];
 
   const preferrence = [
     'behavioral',
@@ -87,7 +86,7 @@ function page() {
       });
 
       const info = {
-        name: data?.name,
+        name: name,
         role, InterviewPreferrence, company, experience, question: res.data.resp
       }
 
@@ -113,6 +112,10 @@ function page() {
     }
   }
 
+  const logout = () => {
+    router.push('/');
+  }
+
   return (
     <>
       <div className="w-full min-h-screen flex flex-col justify-start items-center bg-gradient-to-br from-black to-purple-700 overflow-hidden">
@@ -123,7 +126,8 @@ function page() {
 
           <div className={`w-full relative px-3 py-5 backdrop-blur-3xl rounded-md bg-white/10 lg:rounded-lg h-auto flex flex-col lg:flex-row justify-start md:justify-center items-center lg:gap-5`}>
             <p className="w-full md:w-auto  text-white text-[15px] md:text-2xl lg:text-5xl text-start">Welcome</p>
-            <h1 className="w-full md:w-auto  text-start text-white font-bold text-5xl">Sudipto</h1>
+            <h1 className="w-full md:w-auto  text-start text-white font-bold text-5xl">{name}</h1>
+            <span className="absolute right-5 top-1/2 -translate-y-1/2 p-2 px-5 bg-red-500 text-white cursor-pointer hover:bg-white hover:text-red-500 duration-200 ease-in-out rounded-md active:scale-95" onClick={logout}><FaPowerOff/></span>
           </div>
 
           <p className="w-full text-center text-white text-sm lg:text-lg mt-5">Schedule a new interview</p>
@@ -135,9 +139,9 @@ function page() {
             <ModalBody>
               <ModalContent className="w-full px-3 md:px-3 lg:px-3 overflow-y-auto gap-3 py-2">
                 <h1 className="w-full py-2 text-xl font-bold px-2 text-center">Help us Know More About Your Interview Type</h1>
-                <input onChange={(e) => setRole(e.target.value)} type="text" className=" w-full bg-gray-200 px-3 py-2 rounded-md lg:rounded-lg outline-none" placeholder="Enter your desired role for interview" />
-                <input onChange={(e) => setCompany(e.target.value)} type="text" className=" w-full bg-gray-200 px-3 py-2 rounded-md lg:rounded-lg outline-none" placeholder="Company (if any)" />
-                <input onChange={(e) => setExperience(e.target.value)} type="text" className=" w-full bg-gray-200 px-3 py-2 rounded-md lg:rounded-lg outline-none" placeholder="Years of experience (0 in case of fresher)" />
+                <input onChange={(e) => setRole(e.target.value)} type="text" className=" w-full bg-gray-200 px-3 py-2 rounded-md lg:rounded-lg outline-none" value={role} placeholder="Enter your desired role for interview" />
+                <input onChange={(e) => setCompany(e.target.value)} type="text" className=" w-full bg-gray-200 px-3 py-2 rounded-md lg:rounded-lg outline-none" value={company} placeholder="Company (if any)" />
+                <input onChange={(e) => setExperience(e.target.value)} type="text" className=" w-full bg-gray-200 px-3 py-2 rounded-md lg:rounded-lg outline-none" value={experience} placeholder="Years of experience (0 in case of fresher)" />
 
                 <p className="w-full text-start justify-center items-start mt-2 text-sm">Select your interview preferrence</p>
                 <div className="w-full flex overflow-x-auto justify-start items-start gap-2">
@@ -153,22 +157,14 @@ function page() {
           <hr className="w-full h-[1px] bg-gray-200 my-3" />
 
           {/* past list */}
-          <div className="w-full flex justify-between items-center mt-2 px-3">
+          <div className={`${history.length === 0 ? "hidden" : "block"} w-full flex justify-between items-center mt-2 px-3`}>
             <h1 className="w-auto text-start text-white font-bold text-3xl">Past Sessions</h1>
           </div>
 
-          <div className={`w-full px-5 py-5 mt-2 rounded-md lg:rounded-lg backdrop-blur-3xl bg-white/10 duration-200 transition-all flex justify-start items-center gap-4 ease-in-out h-60`}>
+          <div className={`${history.length > 0 ? "block" : "hidden"} w-full px-5 py-5 mt-2 rounded-md lg:rounded-lg backdrop-blur-3xl bg-white/10 duration-200 transition-all flex justify-start items-center gap-4 ease-in-out h-64`}>
             <div className={`flex w-full h-full overflow-x-auto gap-4 justify-start items-start `}>
-              {array.map((role, index) => {
-                return <div key={index}>
-                  <Drawer>
-                    <DrawerTrigger>
-                      <InterviewBoxPast className={`cursor-pointer`} />
-                    </DrawerTrigger>
-                    <DrawerContent className={`h-[70vh]`}>
-
-                    </DrawerContent>
-                  </Drawer></div>
+              {history.length > 0 && history.map((int, index) => {
+                return <InterviewBoxPast key={index} name={int.name} role={int.role} company={int.company} questions={int?.questions} experience={int.experience} type={int.InterviewPreferrence} feedback={int.feedback} className={`cursor-pointer`} />
               })}
             </div>
           </div>
