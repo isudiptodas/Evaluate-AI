e02bc56301b5d9b2875856a2efbbf3e514ae1fb5
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-22 02:44:51 +0530

    added vapi transcripts in option

--- src/app/user/mock-interview/page.tsx
+++ src/app/user/mock-interview/page.tsx
@@ -7,10 +7,11 @@ import { MdCallEnd } from "react-icons/md";
 import { useSearchParams } from "next/navigation";
 import Vapi from "@vapi-ai/web";
 import { toast } from "sonner";
+import axios from "axios";
 
 interface DATA {
     name: string,
-    interviewPreferrence: string,
+    InterviewPreferrence: string,
     role: string,
     company: string,
     experience: string,
@@ -20,8 +21,10 @@ interface DATA {
 function page() {
 
     const [active, setActive] = useState('');
+    const [name, setName] = useState('');
+    const [questions, setQuestions] = useState('');
     const [loaded, setLoaded] = useState(false);
-    const [messages, setMessages] = useState('');
+    const [messages, setMessages] = useState<{ role: string, message: string }[] | []>([]);
     const [data, setData] = useState<DATA | null>(null);
     const router = useRouter();
     const vapi = useRef<Vapi | null>(null);
@@ -35,6 +38,8 @@ function page() {
         const info = searchParams.get('data');
         const decoded = info ? JSON.parse(decodeURIComponent(info)) : null;
         setData(decoded);
+        setName(decoded.name);
+        setQuestions(decoded.question);
 
         if (info === null) {
             router.push('/user');
@@ -65,7 +70,7 @@ function page() {
                         role: "system",
                         content: `You are a interviewer and i am a candidate. I am here for a mock interview. My name is ${data?.name}
                         and applying for role - ${data?.role} in company - ${data?.company} and my experience is ${data?.experience} 
-                        and my focus on ${data?.interviewPreferrence} type. So start the conversation by first greeting me and then ask 
+                        and my focus on ${data?.InterviewPreferrence} type. So start the conversation by first greeting me and then ask 
                         me questions one by one. Even if the candidate ask for any help or suggestion then do so but dont give away entire answers. 
                         Additionally for the interview to be interesting and engaging add a little human touch by adding some phrases like : umm got it !
                         ohh yess yess, uhh haaaa, hmmm. Below I am providing you the questions. ${data?.questions}`,
@@ -85,7 +90,12 @@ function page() {
 
         vapi.current?.on('call-start', () => {
             toast.dismiss(id);
-            vapi.current?.say(`Hello... I am Selina...and I am going to take your interview for this session are you ready ?`);
+            if (data?.InterviewPreferrence === 'communication') {
+                vapi.current?.say(`Hello... I am Selina...and I am your instructor for this communication session are you ready ?`);
+            }
+            else {
+                vapi.current?.say(`Hello... I am Selina...and I am going to take your interview for this session are you ready ?`);
+            }
             toast.info("Call Started");
         });
         vapi.current?.on('speech-start', () => {
@@ -97,18 +107,55 @@ function page() {
         vapi.current?.on('error', () => {
             toast.error("Internal Error");
         });
+        vapi.current?.on('message', (message) => {
+            if (message.type === 'conversation-update') {
+                const historyMessages: { role: string; message: string }[] = message.conversation.map((msg: any) => ({
+                    role: msg.role,
+                    message: msg.content,
+                }));
+                setMessages((prev) => [...(prev || []), ...historyMessages])
+                //console.log(historyMessages);
+            }
+        });
 
         return () => {
             vapi.current?.stop();
         }
     }, [data]);
 
-    const endCall = () => {
+    const endCall = async () => {
+
         vapi.current?.stop();
-        toast.info("Mock Interview Ended");
-        setTimeout(() => {
-            router.push('/user');
-        }, 2000);
+        setActive('');
+        let toastid = toast.loading("Interview Ended. Generating feedback");
+
+        const stringify = JSON.stringify(messages);
+        //console.log(stringify);
+
+            try {
+                const res = await axios.post(`/api/gemini/feedback`, {
+                    transcripts: stringify
+                });
+
+                if (res.data.success) {
+                    toast.dismiss(toastid);
+                    //console.log(res.data);
+                    const feedback = res.data.resp;
+                    const send = {
+                        name: data?.name,
+                        InterviewPreferrence: data?.InterviewPreferrence,
+                        role: data?.role,
+                        company: data?.company,
+                        experience: data?.experience,
+                        feedback: feedback,
+                        question: questions
+                    }
+                    const encodedData = encodeURIComponent(JSON.stringify(send));
+                    router.push(`/user/mock-interview/feedback?data=${encodedData}`);
+                }
+            } catch (err) {
+                console.log(err);
+            }
     }
 
     return (
@@ -130,7 +177,7 @@ function page() {
                         <div className="h-16 z-30 w-16 bg-white rounded-full flex justify-center items-center overflow-hidden">
                             <MdAccountCircle className="text-6xl" />
                         </div>
-                        <p className="z-30 text-white font-semibold text-lg absolute bottom-5 -translate-y-1/2 left-1/2 -translate-x-1/2">{data?.name} You</p>
+                        <p className="z-30 text-white font-semibold text-lg absolute bottom-5 -translate-y-1/2 left-1/2 -translate-x-1/2 capitalize">{name} (You)</p>
                         <div className={`${active === 'user' ? "block" : "hidden"}  h-16 w-16 rounded-full bg-white left-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 z-10 animate-ping absolute`}></div>
                     </div>
 
